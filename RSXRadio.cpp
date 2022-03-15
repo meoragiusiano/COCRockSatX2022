e1e408cdfb2c8878fa848c9c6ed51d8918dceed5
Andrew Nowakowski <64051143+andrewmnowakowski@users.noreply.github.com>
2022-03-15 15:42:19 -0700

    added an overload to RSXRadio::write() to accept long data type

--- RSXRadio.cpp
+++ RSXRadio.cpp
@@ -2,21 +2,29 @@
 #include "conf.h"
 #include "arduino.h"
 
-
-RSXRadio::RSXRadio(long bps) {
+RSXRadio::RSXRadio(long bps)
+{
   RSXRADIO.begin(bps);
-  message = "";
+  _message = "";
 }
 
-String RSXRadio::read() {
+String RSXRadio::read()
+{
   return RSXRADIO.readString();
 }
 
-void RSXRadio::write(String message) {
-  this->message += message;
+void RSXRadio::write(String message)
+{
+  _message += message;
 }
 
-void RSXRadio::send_message() {
+void RSXRadio::write(long message)
+{
+  _message += String(message);
+}
+
+void RSXRadio::send_message()
+{
   RSXRADIO.println(message);
-  this->message = "";
+  _message = "";
 }
--- RSXRadio.h
+++ RSXRadio.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "arduino.h"
 
+class RSXRadio
+{
+private:
+  String _message;
 
-class RSXRadio {
-  private:
-    String message;
-  public:
-    RSXRadio(long);
-    String read();
-    void write(String message);
-    void send_message();
+public:
+  RSXRadio(long);
+  String read();
+  void write(String message);
+  void write(long message);
+  void send_message();
 };
